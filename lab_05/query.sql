0df50552f49b046804a0be8ffe6f45c86b594db4
arina-kaa <Arina-kaa@inbox.ru>
2020-04-10 20:21:50 +0300

    Лабораторная работа №6

--- lab_05/query.sql
+++ lab_05/query.sql
@@ -50,19 +50,19 @@ SELECT SETVAL('room_in_booking_id_room_in_booking_seq', (SELECT MAX(id_room_in_b
 
 -- 1. Добавить внешние ключи
 ALTER TABLE booking
-    ADD CONSTRAINT fk_booking_client FOREIGN KEY (id_client) REFERENCES client (id_client);
+ADD CONSTRAINT fk_booking_client FOREIGN KEY (id_client) REFERENCES client (id_client);
 
 ALTER TABLE room
-    ADD CONSTRAINT fk_room_hotel FOREIGN KEY (id_hotel) REFERENCES hotel (id_hotel);
+ADD CONSTRAINT fk_room_hotel FOREIGN KEY (id_hotel) REFERENCES hotel (id_hotel);
 
 ALTER TABLE room
-    ADD CONSTRAINT fk_room_room_category FOREIGN KEY (id_room_category) REFERENCES room_category (id_room_category);
+ADD CONSTRAINT fk_room_room_category FOREIGN KEY (id_room_category) REFERENCES room_category (id_room_category);
 
 ALTER TABLE room_in_booking
-    ADD CONSTRAINT fk_room_in_booking_booking FOREIGN KEY (id_booking) REFERENCES booking (id_booking);
+ADD CONSTRAINT fk_room_in_booking_booking FOREIGN KEY (id_booking) REFERENCES booking (id_booking);
 
 ALTER TABLE room_in_booking
-    ADD CONSTRAINT fk_room_in_booking_room FOREIGN KEY (id_room) REFERENCES room (id_room);
+ADD CONSTRAINT fk_room_in_booking_room FOREIGN KEY (id_room) REFERENCES room (id_room);
 
 -- 2. Выдать информацию о клиентах гостиницы “Космос”, проживающих в номерах
 -- категории “Люкс” на 1 апреля 2019 г.
